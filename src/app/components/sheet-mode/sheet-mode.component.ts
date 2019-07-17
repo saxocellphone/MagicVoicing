79473f66508f6c9613de14dc42712764b810a873
oakleyaidan21 <oakleyaidan@gmail.com>
2019-07-17 14:28:17 -0400

    added hover function for chords

--- src/app/components/sheet-mode/sheet-mode.component.ts
+++ src/app/components/sheet-mode/sheet-mode.component.ts
@@ -16,12 +16,14 @@ export class SheetModeComponent implements OnInit {
   tune: Tune;
   name: string;
   measures: number;
+  count: number;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private service: DataService) { }
 
   ngOnInit() {
+    this.count = 0;
     this.tune$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) =>
         this.service.getTune(params.get('name')))
@@ -31,6 +33,26 @@ export class SheetModeComponent implements OnInit {
       this.tune = t;
       this.measures = this.tune.measures.length;
     });
+    
+  }
+
+  mouseEnter(div: string) {
+    var chords = div.split(" ");
+    for(var i = 0; i < chords.length; i++) {
+      if(chords[i] != "undefined") {
+        console.log('measure[' + i +']: ' + chords[i]);
+      }
+    }
+  }
+
+  mouseLeave(div : string) {
+    //remove chord display from page
+    // console.log('mouse leave: ' + div);
+    // console.log('leave name: ' + this.name);
+    // console.log('leave measures: '  + this.measures);
+    this.count++;
+    console.log('leave: '+this.count);
+
   }
 
 }
