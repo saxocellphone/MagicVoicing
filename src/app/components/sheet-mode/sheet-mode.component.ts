c8700ddf76509bf2a55dc13a4c5881f9e9de6b33
Victor Nazzaro <nazzav@rpi.edu>
2019-08-15 17:23:27 -0400

    abcjs work

--- src/app/components/sheet-mode/sheet-mode.component.ts
+++ src/app/components/sheet-mode/sheet-mode.component.ts
@@ -4,6 +4,9 @@ import { DataService } from 'src/app/data.service';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Tune } from 'src/app/models/tune';
+import * as ABCJS from 'abcjs';
+import * as octavian from 'octavian';
+import MusicUtils from 'src/app/utils';
 
 @Component({
   selector: 'app-sheet-mode',
@@ -29,8 +32,22 @@ export class SheetModeComponent implements OnInit {
     this.name = this.route.snapshot.paramMap.get('name');
     this.service.getTune(this.name).subscribe(t => {
       this.tune = t;
+      const el: HTMLElement = document.getElementById('test');
+      const abc = 'X: 1\n' +
+                  `T: ${t.name}\n` +
+                  `M: 4/4\n` +
+                  `L: 1/2\n` +
+                  `Q: 1/4=${t.bpm}\n` +
+                  `C: ${t.composer}\n` +
+                  `S: \n` +
+                  `R: ${t.style}\n` +
+                  `K:C\n` +
+                  `${MusicUtils.generateABCString(t.measures)}`;
+      const tuneObjectArray = ABCJS.renderAbc('notation', abc);
+
       this.measures = this.tune.measures.length;
     });
+    console.log(MusicUtils.parseChords('C7'));
   }
 
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,11 +1,19 @@
 import { Chord } from './models/chord';
 import * as _ from 'lodash';
+import * as octavian from 'octavian';
 
 export enum CHORD_TYPES {
     MAJOR = 'maj',
+    MAJOR6 = 'maj6',
+    MAJOR7 = 'maj7',
+    MAJOR7b5 = 'maj7b5',
+    MAJOR7s5 = 'maj7#5',
     MINOR = 'min',
-    DIMINISHED = 'dim',
-    DOMINANT = 'dom7'
+    MINOR6 = 'min6',
+    MINOR7 = 'min7',
+    DOMINANT7 = 'dom7',
+    DIMINISHED7 = 'dim7',
+    HALF_DIMINISHED = 'm7b5'
 }
 
 export default class MusicUtils {
@@ -18,6 +26,36 @@ export default class MusicUtils {
         });
     }
 
+    static generateABCString(chords: string[][]): string {
+        let abcstring: string = '';
+        // tslint:disable-next-line: forin
+
+        _.forEach(chords, (measure: string[], index: number) => {
+            abcstring += '|';
+            _.forEach(measure, (chord: string) => {
+                abcstring += '\"';
+                abcstring += chord;
+                abcstring += '\"';
+                abcstring += '[';
+                const notes: octavian.Chord = this.parseChord(chord).notes;
+                for(const note in notes){
+                    if (notes[note].modifier === '#') {
+                        abcstring += '^';
+                    } else if (notes[note].modifier === 'b') {
+                        abcstring += '_';
+                    }
+                    abcstring += notes[note].letter;
+                }
+                abcstring += ']';
+            });
+            if ((index + 1) % 4 === 0) {
+                abcstring += '|\n';
+            }
+        });
+        abcstring += '||';
+        return abcstring;
+    }
+
     /**
      * Parse a string into a Chord type.
      *
@@ -25,10 +63,11 @@ export default class MusicUtils {
      * @throws Error if the chord is not recognized
      * @returns a Chord type
      */
-    static parseChord(chord: string): Chord {
+    static parseChord(chord: string): octavian.Chord {
         const rootMatch = chord.match(/[A-G][#b]?/g);  // includes sharp/flat
-        // M is major, m is minor, a is augmented, d is diminished, default is dominant
-        const typeMatch = chord.match(/[Mmad]/g) || [CHORD_TYPES.DOMINANT];
+        // M is major, m is minor, a is augmented, d is diminished,
+        // h is half-deminished, default is major
+        const typeMatch = chord.match(/[Mmadh]/g) || [CHORD_TYPES.MAJOR];
         const modifiersMatch = chord.match(/(?<=\()[#b]?\d+(?=\))/g); // All the numbers after the chord
 
         if (!rootMatch || rootMatch.length !== 1) {
@@ -38,15 +77,37 @@ export default class MusicUtils {
         if (!typeMatch || typeMatch.length !== 1) {
             throw new Error(`The chord type in ${chord} is not recognized.`);
         }
+        let type: CHORD_TYPES;
+        switch (typeMatch[0]) {
+            case '^':
+                type = CHORD_TYPES.MAJOR7;
+                break;
+            case '-':
+                switch (modifiersMatch[0]) {
+                    case '6':
+                        type = CHORD_TYPES.MINOR6;
+                        break;
+                    case '7':
+                        type = CHORD_TYPES.MINOR7;
+                        break;
+                    default:
+                        type = CHORD_TYPES.MINOR;
+                        break;
+                }
+                break;
+            default:
+                if (modifiersMatch){
+                    type = CHORD_TYPES.MAJOR6;
+                } else {
+                    type = CHORD_TYPES.DOMINANT7;
+                }
+                break;
+        }
 
-        const root = rootMatch[0];
-        const type = typeMatch[0];
+        const root = rootMatch[0] + '3';
+        // const type = typeMatch[0];
         const modifiers = modifiersMatch;
-        const newChord: Chord = {
-            root,
-            type,
-            modifiers
-        };
+        const newChord = new octavian.Chord(root, type);
         return newChord;
     }
 }
